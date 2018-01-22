fb532d5b48ac9d45c01759cfd96b95e8f3deee3c
MarcFarkouh <Marc.Farkouh19@alumni.loyola.ca>
2018-01-22 13:59:49 -0500

    Add files via upload

--- Java Script Schedule.js
+++ Java Script Schedule.js
@@ -1,9 +1,4 @@
-var backround;
-var "physical education" = document.getElementById("my favouirte class").style.backround = green;
-
-switch var(physical education){
-	case "Physical Education";
-    backround = "green";
-break;
-	
+function Colour() {
+    document.getElementById("english").style.color = "blue";
 }
+
