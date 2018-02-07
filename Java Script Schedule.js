4c0fa1d122b65cc12908cbdb0ea6c9d39d61a115
MarcFarkouh <Marc.Farkouh19@alumni.loyola.ca>
2018-02-07 10:53:37 -0500

    Add files via upload

--- Java Script Schedule.js
+++ Java Script Schedule.js
@@ -1,6 +1,9 @@
-swicth (class) {
+function
+
+var Class = 'documentgetElementsByClassName';
+swicth (Class) {
 	case 'English':
-		console.log ("hey")
+		console.log ("Indie Reading")
 	break;
 	
 	case 'Math':
@@ -11,5 +14,39 @@ swicth (class) {
 		console.log ("Bonjour")
 	break;
 	
-	case '
+	case 'Religion':
+		console.log ("Gentlemen")
+	break;
+	
+	case 'Computer Programming':
+		console.log ("Computers")
+	break;
+	
+	case 'Music':
+		console.log ("Hey Hey")
+	break;
+	
+	case 'History':
+		console.log ("Hand cramps")
+	break;
+	
+	case 'Science':
+		console.log ("Stoicheomitry")
+	break;
+	
+	case 'Speech':
+		console.log ("Speaking")
+	break;
+	
+	case 'Level Assembly':
+		console.log ("Boring")
+	break;
+	
+	case 'Homeroom':
+		console.log ("One a cycle")
+	break;
+	
+	case 'Advisors':
+		console.log ("I love this class")
+	break;
 }
