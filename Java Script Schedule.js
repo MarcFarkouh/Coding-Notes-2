f55a86c8ff1de556014cc564bcfb44d5ce716489
MarcFarkouh <Marc.Farkouh19@alumni.loyola.ca>
2018-02-13 16:51:17 -0500

    Update Java Script Schedule.js

--- Java Script Schedule.js
+++ Java Script Schedule.js
@@ -1,52 +1,88 @@
-function
-
-var Class = 'documentgetElementsByClassName';
+let m= document.getElementById("something").innerHTML= Date{}
+var selectedclass:
+function highlightStyle{}
 swicth (Class) {
 	case 'English':
+		selectedClass="English";
+		break;
+	default:
 		console.log ("Indie Reading")
 	break;
 	
 	case 'Math':
+		selectedClass="Math";
+		break;
+	default:
 		console.log ("Do math")
 	break;
 	
 	case 'French':
+		selectedClass="French";
+		break;
+	default:
 		console.log ("Bonjour")
 	break;
 	
 	case 'Religion':
+		selectedClass="Religion";
+		break;
+	default:
 		console.log ("Gentlemen")
 	break;
 	
 	case 'Computer Programming':
+		selectedClass="Computer Programming";
+		break;
+	default:
 		console.log ("Computers")
 	break;
 	
 	case 'Music':
+		selectedClass="Music";
+		break;
+	default:
 		console.log ("Hey Hey")
 	break;
 	
 	case 'History':
+		selectedClass="History";
+		break;
+	default:
 		console.log ("Hand cramps")
 	break;
 	
 	case 'Science':
+		selectedClass="Science";
+		break;
+	default:
 		console.log ("Stoicheomitry")
 	break;
 	
 	case 'Speech':
+		selectedClass="Speech";
+		break;
+	default:
 		console.log ("Speaking")
 	break;
 	
 	case 'Level Assembly':
+		selectedClass="Level Assembly";
+		break;
+	default:
 		console.log ("Boring")
 	break;
 	
 	case 'Homeroom':
+		selectedClass="Homeroom";
+		break;
+	default:
 		console.log ("One a cycle")
 	break;
 	
 	case 'Advisors':
+		selectedClass="Advisors";
+		break;
+	default:
 		console.log ("I love this class")
 	break;
 }
